f706bae7839823272108bbc45a2faf0c66691fa4
Maxim <54582514+MisCam@users.noreply.github.com>
2022-03-16 12:34:38 +0400

    change

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 const Mediator = require('./application/modules/Mediator');
-const PhoneBook = require("./application/modules/phonebook/PhoneBook");
 const UserManager = require("./application/modules/userManager/UserManager");
-const NoteBook = require("./application/modules/notebook/NoteBook");
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 io.on('connection', socket => {
     console.log('connected ', socket.id);
@@ -20,13 +26,6 @@ new UserManager({ mediator });
 
 const Router = require("./application/router/Router");
 const router = new Router({ mediator });
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
 app.use(express.static(__dirname + "/public"));
 app.use("/", router);
 
--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 const canv = document.getElementById('canv');
 const socket = io('http://localhost:3000');
-socket.emit(MESSAGES.NEW_MESSAGE, { name, message });
-socket.on(MESSAGES.NEW_MESSAGE, newMessage);
+/* socket.emit(MESSAGES.NEW_MESSAGE, { name, message });
+socket.on(MESSAGES.NEW_MESSAGE, newMessage); */
 let config = {
   type: Phaser.AUTO,
   width: 600,
@@ -26,7 +26,8 @@ let config = {
 
 document.getElementById('start_game').addEventListener('click', async function() {
   const nick = document.getElementById('login_input').value;
-  fetch(`http://localhost:3000/login/${nick}`);
+  //fetch(`http://localhost:3000/login/${nick}`);
+  canv.innerHTML = '';
   let game = new Phaser.Game(config);
 });
 
@@ -35,33 +36,32 @@ function preload() {}
 function create() {
   moveKeys = this.input.keyboard.createCursorKeys();
   this.add.rectangle(0, 0, config.width*2, config.height*2, '0xffffff');
-  console.log(getPlayers());
   player = this.physics.add.sys.add.circle(100, 100, 25, '0xff0000');
   const moving = setInterval(() => {
-    move(player.x, player.y);
+    //move(player.x, player.y);
   }, 200);
 }
 
 function update() {
   if (moveKeys.left.isDown) {
-    player.x--;
+    player.x -= 1.5;
   } else if (moveKeys.right.isDown) {
-    player.x++;
+    player.x += 1.5;
   }
   if (moveKeys.up.isDown) {
-    player.y--;
+    player.y -= 1.5;
   } else if (moveKeys.down.isDown) {
-    player.y++;
+    player.y += 1.5;
   }
 
 }
 
 function move(x, y){
   const nick = document.getElementById('login_input').value;
-  fetch(`http://localhost:3000/players/move/${nick}/${x}/${y}`);
+  //fetch(`http://localhost:3000/players/move/${nick}/${x}/${y}`);
 }
 
 async function getPlayers(){
-  const answer = await fetch(`http://localhost:3000/players/get`);
-  return answer.json();
+  //const answer = await fetch(`http://localhost:3000/players/get`);
+  //return answer.json();
 }
